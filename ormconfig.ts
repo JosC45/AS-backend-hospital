dbdef9b73ba02ce3d4bdd9c7f810ddde09327fac
Axel <20b3010455@untels.edu.pe>
2025-11-10 14:32:24 -0500

    cambios para prod v2

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,25 @@
-import { ENTITIES } from "src/entities";
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
+const requiredEnvVars = [
+    'DB_HOST',
+    'DB_PORT',
+    'DB_USERNAME',
+    'DB_PASSWORD',
+    'DB_DATABASE'
+];
+
+for (const varName of requiredEnvVars) {
+    if (!process.env[varName]) {
+        throw new Error(`Error de configuración: La variable de entorno requerida "${varName}" no está definida.`);
+    }
+}
 
 export default new DataSource({
     type: 'mysql',
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
+    port: parseInt(process.env.DB_PORT!, 10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
