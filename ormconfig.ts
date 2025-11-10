d6a80fed203e5bad49fc28433f9536f655293ae4
Axel <20b3010455@untels.edu.pe>
2025-11-10 14:28:59 -0500

    cambios para prod

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,13 +4,19 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default new DataSource({
-    type:'mysql',
+    type: 'mysql',
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER ,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: ENTITIES, 
-    migrations: ['src/migrations/*.ts'],
-    synchronize: true,
-})
+    database: process.env.DB_DATABASE,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    migrations: [__dirname + '/migrations/*{.ts,.js}'],
+    ssl: true, 
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
